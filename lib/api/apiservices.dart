a60561edbb22c9c12909ed643def452ae3622ace
aswin531 <aswinkvaswinkv1@gmail.com>
2024-05-15 20:39:53 +0530

    added model

---
+++ lib/api/apiservices.dart
@@ -0,0 +1,8 @@
+class APIEndPoints {
+  final String baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=india&appid=fb8f2cb6fff3e4fd347751820c01a955&units=metric";
+  final String cityEndPoint = "";
+  final String lanlangEndPoint = "";
+
+  final unit = "";
+
+}
---
+++ lib/api/constants.dart
@@ -0,0 +1,1 @@
+const apiKey = "AIzaSyC5WbXMkrMAOw4IB9LaR5q7Mv442vpko6U";
